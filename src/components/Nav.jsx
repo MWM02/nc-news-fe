22c44a72c9a5b4ca8fe9a69c249dc24dd9f5b550
MWM02 <wijem004@gmail.com>
2025-04-01 15:06:57 +0100

    creates Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Nav = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
+      <Link to={`/articles`}>Home</Link>
       <>Post Article</>
     </nav>
   );
