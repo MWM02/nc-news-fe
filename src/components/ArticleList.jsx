692d059eb26b02ae091573f96a0600f5a4b3bc51
MWM02 <wijem004@gmail.com>
2025-04-01 15:06:01 +0100

    adds pagination logic and custom hook for data fetching

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,23 +1,60 @@
-import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { getArticles } from "../api";
 import { ArticleCard } from "./ArticleCard";
+import useApiRequest from "../custom_hooks/useApiRequest";
 
 export const ArticleList = () => {
-  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [resultsPerPage, setResultsPerPage] = useState(10);
+  const { data, isLoading, error } = useApiRequest(
+    getArticles,
+    page,
+    resultsPerPage
+  );
+  const lastPage = data ? Math.ceil(data.total_count / resultsPerPage) : null;
 
-  useEffect(() => {
-    getArticles().then((articles) => {
-      setArticles(articles);
-    });
-  }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
 
   return (
-    <section>
-      <ol>
-        {articles.map((article) => (
-          <ArticleCard key={article.article_id} article={article} />
-        ))}
-      </ol>
-    </section>
+    <>
+      <section>
+        <ol className="article-list">
+          {data.articles.map((article) => (
+            <ArticleCard key={article.article_id} article={article} />
+          ))}
+        </ol>
+      </section>
+      <div className="page-buttons">
+        <Link to={`/articles?p=${page - 1}&limit=${resultsPerPage}`}>
+          <button
+            className="previous-page-button"
+            onClick={() => {
+              setPage(page - 1);
+            }}
+            disabled={page === 1}
+          >
+            {"<"}
+          </button>
+        </Link>
+        <Link to={`/articles?p=${page + 1}&limit=${resultsPerPage}`}>
+          <button
+            className="next-page-button"
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            disabled={page === lastPage}
+          >
+            {">"}
+          </button>
+        </Link>
+      </div>
+    </>
   );
 };
