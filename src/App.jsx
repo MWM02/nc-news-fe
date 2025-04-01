d9cf04f71a2ccfa66ae92b94bcd36b80d9eb1cad
MWM02 <wijem004@gmail.com>
2025-04-01 15:02:10 +0100

    seperates Nav component from Header and updates path for article list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Header } from "./components/Header";
+import { Nav } from "./components/Nav";
 import { ArticleList } from "./components/ArticleList";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Nav />
       <Routes>
-        <Route path="/" element={<ArticleList />} />
+        <Route path="/articles" element={<ArticleList />} />
       </Routes>
     </>
   );
