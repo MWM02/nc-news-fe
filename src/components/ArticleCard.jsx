c3f8b4e50cd976fdfaff547a55aeabbb3d5d5b6e
MWM02 <wijem004@gmail.com>
2025-04-02 08:50:47 +0100

    adds link to Article from ArticleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 export const ArticleCard = ({ article }) => {
   return (
-    <li>
-      <h2>{article.title}</h2>
-      <img src={article.article_img_url}></img>
-      <p>by {article.author}</p>
-      <p>{article.votes} votes</p>
-      <p>{article.comment_count} comments</p>
-    </li>
+    <Link to={`/articles/${article.article_id}`}>
+      <li>
+        <h2>{article.title}</h2>
+        <img src={article.article_img_url}></img>
+        <p>by {article.author}</p>
+        <p>{article.votes} votes</p>
+        <p>{article.comment_count} comments</p>
+      </li>
+    </Link>
   );
 };
