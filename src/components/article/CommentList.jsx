8ad9f1f64164d2c2dc5f5cb99e5754664a4e0040
MWM02 <wijem004@gmail.com>
2025-04-02 15:25:30 +0100

    creates CommentList

---
+++ src/components/article/CommentList.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { getComments } from "../../api";
+import { CommentCard } from "./CommentCard";
+
+export const CommentList = ({ article_id, comment_count }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [page, setPage] = useState(1);
+  const [commemtsPerPage, setCommemtsPerPage] = useState(3);
+  const lastPage = Math.ceil(comment_count / commemtsPerPage);
+
+  useEffect(() => {
+    getComments(article_id, page, commemtsPerPage)
+      .then(({ data: { comments } }) => {
+        setComments(comments);
+        setPage(page + 1);
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const loadMore = () => {
+    setIsLoading(true);
+    if (page !== null) {
+      getComments(article_id, page, commemtsPerPage)
+        .then(({ data: { comments } }) => {
+          setComments((prevComments) => [...prevComments, ...comments]);
+          setPage(page + 1);
+        })
+        .catch((error) => setError(error.message))
+        .finally(() => setIsLoading(false));
+    }
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return (
+    <>
+      <section>
+        <h4 id="comment-section-header">Comments</h4>
+        <ol>
+          {comments.map((comment) => (
+            <CommentCard key={comment.comment_id} comment={comment} />
+          ))}
+        </ol>
+        {lastPage !== page - 1 && page !== null && (
+          <div className="load-more-button">
+            <button onClick={loadMore}>Load More</button>
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
