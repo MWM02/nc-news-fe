81b39b49874ad8901ac662c086df330255b455d9
MWM02 <wijem004@gmail.com>
2025-04-06 16:59:10 +0100

    adds time formatter

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,12 @@
 export const capitalise = (str) =>
   String(str)[0].toUpperCase() + String(str).slice(1).toLowerCase();
+
+export const timeFormatted = (date) => {
+  const newDate = new Date(date);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return newDate.toLocaleDateString(undefined, options);
+};
