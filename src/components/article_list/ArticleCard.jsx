285ead2990cbf2ddb0d67ad04a8bbeae3e938b4f
MWM02 <wijem004@gmail.com>
2025-04-28 17:38:07 +0100

    minor changes

--- src/components/article_list/ArticleCard.jsx
+++ src/components/article_list/ArticleCard.jsx
@@ -6,14 +6,17 @@ export const ArticleCard = ({ article }) => {
     <Link to={`/articles/${article.article_id}`} className="article-card">
       <h3 className="article-card__title">{article.title}</h3>
       <h4 className="article-card__topic">{article.topic.toUpperCase()}</h4>
-      <img className="article-card__image" src={article.article_img_url}></img>
+      <img
+        className="article-card__image"
+        src={article.article_img_url || null}
+      ></img>
       <p className="article-card__author">by {article.author}</p>
       <p className="article-card__votes">
         {article.votes} {article.votes === 1 ? "vote" : "votes"}
       </p>
       <p className="article-card__comments">
-        {article.comment_count}{" "}
-        {article.comment_count === 1 ? "comment" : "comments"}
+        {article.comment_count}
+        {article.comment_count === 1 ? " comment" : " comments"}
       </p>
     </Link>
   );
